47345258647a6e575406bf89a5fbbf1b26904ad3
Samuel Michaud <samuel.michaud@gmail.com>
2015-12-08 23:39:19 +0100

    Add appcache plugin for optimized cache

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const Clean = require('clean-webpack-plugin');
+const AppCachePlugin = require('appcache-webpack-plugin');
 const buildRep = process.env.npm_package_config_buildRep;
 
 module.exports = {
@@ -23,26 +24,30 @@ module.exports = {
     new ExtractTextPlugin('src/styles.[hash].css', {
       allChunks: true
     }),
+    new AppCachePlugin({
+      fallback: ['index.html'],// you may want to disable this line if your app can't fully work offline
+      exclude: ['index.html']
+    }),
     new Clean([buildRep])
   ],
   module: {
     loaders: [
-        {
-            test: /\.scss$/,
-            loader: ExtractTextPlugin.extract('style', 'css!sass')
-        },
-        {
-          test: /\.js$/,
-          loaders: ['react-hot', 'babel'],
-          include: path.join(__dirname, 'src')
-        },
-        {
-            test: /\.(ico|jpe?g|png|gif|svg)$/,
-            loaders: [
-                'file?name=[path][name].[hash].[ext]',
-                'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-            ]
-        }
+      {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract('style', 'css!sass')
+      },
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+          test: /\.(ico|jpe?g|png|gif|svg)$/,
+          loaders: [
+              'file?name=[path][name].[hash].[ext]',
+              'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          ]
+      }
     ]
   }
 };
